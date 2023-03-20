3ed4d2cb6b4b7062621b67daa8f495be22899c0f
Michał Świątek <michalswiatek92@gmail.com>
2023-03-20 13:46:21 +0100

    Add files

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+import Add from './components/Add/Add';
+import Header from './components/Header/Header';
+import { useEffect, useState } from 'react';
+import Form from './components/Form/Form';
+import TodoList from './components/ToDoList/ToDoList';
+import Time from './components/Time/Time';
+
+function App() {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  const filterHandler = () => {
+    switch (status) {
+      case 'completed':
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+
+  return (
+    <div className="App">
+      <Header />
+      <Time />
+      <Add />
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <TodoList
+        filteredTodos={filteredTodos}
+        setTodos={setTodos}
+        todos={todos}
+      />
+    </div>
+  );
+}
+
+export default App;
